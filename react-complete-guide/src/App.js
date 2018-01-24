d219cf03910045e1cb4355fc6ff435190dfb88c1
Randy Davis <randy_davis@outlook.com>
2018-01-23 23:49:01 -0600

    Refactor our persons toggle to use a more 'Javascript Way' of rendering the content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -47,20 +47,12 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
+		}
+		let persons = null;
 
-		return (
-			<div className="App">
-				<h1>Hi, I'm a React App</h1>
-				<p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}
-        >
-        Toggle Persons
-				</button>
-				{this.state.showPersons ?
-					<div>
+		if (this.state.showPersons) {
+			persons = (
+				<div>
 						<Person
 							name={this.state.persons[0].name}
 							age={this.state.persons[0].age}
@@ -77,8 +69,21 @@ class App extends Component {
 							name={this.state.persons[2].name}
 							age={this.state.persons[2].age}
 						/>
-					</div> : null
-				}
+					</div>
+			);
+		}
+
+		return (
+			<div className="App">
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}
+        >
+        Toggle Persons
+				</button>
+				{ persons }
 			</div>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'));
