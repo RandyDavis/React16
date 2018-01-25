0b3fcdfe34e51b168adbabb6733678d5786fb68f
Randy Davis <randy_davis@outlook.com>
2018-01-24 21:16:19 -0600

    Made list flexible by updating change event to target data bound to it

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -20,14 +20,21 @@ class App extends Component {
 		this.setState({persons: persons});
 	}
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-				{ name: 'Max', age: 28 },
-				{ name: event.target.value, age: 29 },
-				{ name: "Stephanie", age: 26 }
-			]
-    });
+  nameChangedHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex(p => {
+			return p.id === id;
+		});
+
+		const person = {
+			...this.state.persons[personIndex]
+		}; // Alternative way is shown directly below
+		// const person = Object.assign({}, this.state.persons[personIndex]);
+
+		person.name = event.target.value;
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
+
+		this.setState({ persons: persons })
 	};
 
 	togglePersonsHandler = () => {
@@ -53,7 +60,8 @@ class App extends Component {
 							click={() => this.deletePersonHandler(index)}
 							name={person.name}
 							age={person.age}
-							key={person.id} />
+							key={person.id}
+							changed={(event) => this.nameChangedHandler(event, person.id)} />
 						})
 					}
 				</div>
