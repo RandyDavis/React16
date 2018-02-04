16183c92de2b6301829d3843878d939e9bd20f8c
Randy Davis <randy_davis@outlook.com>
2018-02-03 22:28:31 -0600

    ErrorBoundary example

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./App.css";
 import Person from "./Person/Person";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
 	state = {
@@ -43,22 +44,22 @@ class App extends Component {
 	};
 
 	render() {
-
 		let persons = null;
-		let btnClass = '';
+		let btnClass = "";
 
 		if (this.state.showPersons) {
 			persons = (
 				<div>
 					{this.state.persons.map((person, index) => {
 						return (
-							<Person
-								click={() => this.deletePersonHandler(index)}
-								name={person.name}
-								age={person.age}
-								key={person.id}
-								changed={event => this.nameChangedHandler(event, person.id)}
-							/>
+							<ErrorBoundary key={person.id}>
+								<Person
+									click={() => this.deletePersonHandler(index)}
+									name={person.name}
+									age={person.age}
+									changed={event => this.nameChangedHandler(event, person.id)}
+								/>
+							</ErrorBoundary>
 						);
 					})}
 				</div>
@@ -79,9 +80,7 @@ class App extends Component {
 			<div className={classes.App}>
 				<h1>Hi, I'm a React App</h1>
 				<p className={assignedClasses.join(" ")}>This is really working!</p>
-				<button
-					className={ btnClass }
-					onClick={this.togglePersonsHandler}>
+				<button className={btnClass} onClick={this.togglePersonsHandler}>
 					Toggle Persons
 				</button>
 				{persons}
