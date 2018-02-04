aa102ecdb390f7632a529017e613f88cefd84693
Randy Davis <randy_davis@outlook.com>
2018-02-03 21:56:35 -0600

    Show how to add pseudo selectors

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -43,16 +43,9 @@ class App extends Component {
 	};
 
 	render() {
-		const style = {
-			backgroundColor: "green",
-			color: "white",
-			font: "inherit",
-			border: "1px solid blue",
-			padding: "8px",
-			cursor: "pointer"
-		};
 
 		let persons = null;
+		let btnClass = '';
 
 		if (this.state.showPersons) {
 			persons = (
@@ -71,7 +64,7 @@ class App extends Component {
 				</div>
 			);
 
-			style.backgroundColor = "red";
+			btnClass = classes.Red;
 		}
 
 		const assignedClasses = [];
@@ -86,7 +79,9 @@ class App extends Component {
 			<div className={classes.App}>
 				<h1>Hi, I'm a React App</h1>
 				<p className={assignedClasses.join(" ")}>This is really working!</p>
-				<button style={style} onClick={this.togglePersonsHandler}>
+				<button
+					className={ btnClass }
+					onClick={this.togglePersonsHandler}>
 					Toggle Persons
 				</button>
 				{persons}
