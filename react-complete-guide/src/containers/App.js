b2473ff3a8aebee60de3105a48311d2c06ead352
Randy Davis <randy_davis@outlook.com>
2018-02-27 22:51:06 -0600

    Update to use a different pattern for Higher Order Components but still need to address state

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
-import WithClass from "../hoc/WithClass";
+import Aux from "../hoc/Aux";
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
 	constructor(props) {
@@ -98,7 +99,7 @@ class App extends PureComponent {
 
 
 		return (
-			<WithClass classes={classes.App}>
+			<Aux>
 				<button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
 				<Cockpit
 					appTitle={this.props.title}
@@ -107,10 +108,10 @@ class App extends PureComponent {
 					clicked={this.togglePersonsHandler}
 				/>
 				{persons}
-			</WithClass>
+			</Aux>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'));
 	}
 }
 
-export default App;
+export default withClass(App, classes.App);
