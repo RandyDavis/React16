12670e986a1a3b0f83646a51520a466a07be8029
Randy Davis <randy_davis@outlook.com>
2018-02-27 23:29:54 -0600

    Update HOCs to use props correctly; Update state to use prevState with setState;

--- react-complete-guide/src/hoc/WithClass.js
+++ react-complete-guide/src/hoc/WithClass.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// Using a HIGHER ORDER COMPONENT
+// const withClass = (WrappedComponent, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     )
+// }
+
+// Using a normal STATEFULcomponent
 const withClass = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent />
-        </div>
-    )
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            )
+        }
+    }
 }
 
 export default withClass;
