e21fbd78be31af77bd825f018281699b01d6f12f
Randy Davis <randy_davis@outlook.com>
2018-03-10 00:47:48 -0600

    Update Modal click event

--- burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchaseable: false
+        purchaseable: false,
+        purchasing: false
     }
 
     updatePurchaseState (ingredients) {
@@ -69,6 +70,10 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -78,7 +83,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing} >
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -87,6 +92,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={ this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchaseable={this.state.purchaseable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice}
                 />
             </Aux>
